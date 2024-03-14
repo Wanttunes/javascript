39d6cc4963298b1cd7f628c3e104df77b97603a3
Willian Antunes <56939146+Wanttunes@users.noreply.github.com>
2024-03-14 19:10:48 -0300

    Update de acessibilidade

--- exercicios/aula12/ex001.js
+++ exercicios/aula12/ex001.js
@@ -1,10 +1,9 @@
-var idade = 16
+var idade = 43
+console.log(`Você tem ${idade} anos.`)
 if(idade < 16) {
     console.log('Não Vota')
-}else{
-    if(idade < 18) {
+}else if(idade < 18 || idade > 65) {
         console.log('Voto Opcional')
     } else{
         console.log('Voto Obrigatório')
-    }
 }
