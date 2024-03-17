de363cc6b3297542bccf111f10f3d30474f34c0d
Willian Antunes <56939146+Wanttunes@users.noreply.github.com>
2024-03-17 19:36:51 -0300

    Update for studies

--- exercicios/aula17/ex001/script.js
+++ exercicios/aula17/ex001/script.js
@@ -1,46 +1,35 @@
-function verificar(){
-    var anon = document.getElementById('iano')
-    var res = document.getElementById('msg')
-    var data = new Date
-    var anoA = data.getFullYear()
-
-    if(Number(anon.value) == 0 || Number(anon.value) >= anoA ){
-        window.alert(`[ERRO]Tente novamente!`)
-        return
-    }else{
-        var sexo = document.getElementsByName('sex')
-        var idade = anoA - Number(anon.value)
-        var gênero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-
-        if (sexo[0].checked){
-        gênero = ('um Homem')
-        if(idade >= 1 && idade <= 17) {
-            img.setAttribute('src','imagens/homem-crianca.jpg')
-
-        } else if (idade <= 50){
-            img.setAttribute('src','imagens/homem-adulto.jpg')
-        } else {
-            img.setAttribute('src','imagens/homem-velho.jpg')
-        }
-    } else if (sexo[1].checked){
-        gênero = ('uma Mulher')
-        if(idade >= 1 && idade <= 17) {
-            res.innerText = `Detectamos ${gênero} com ${idade} anos.`
-            img.setAttribute('src','imagens/mulher-crianca.jpg')
-        } else if (idade <= 50){
-            res.innerText = `Detectamos ${gênero} com ${idade} anos.`
-            img.setAttribute('src','imagens/mulher-adulta.jpg')
-        } else {
-            res.innerText = `Detectamos ${gênero} com ${idade} anos.`
-            img.setAttribute('src','imagens/mulher-velha.jpg')
-        }
-    } 
+function contar(){
+    let inui = document.getElementById('iinicio')
+    let ifim = document.getElementById('ifim')
+    let ipasso = document.getElementById('ipasso')
+    let res = document.getElementById('res')
     
-       
+    if (inui.value.length == 0 || ifim.value.length == 0 || ipasso.value.length == 0 ){ 
+        //Confirmação de dados
+        window.alert('[ERRO] Digite alguma valor para começar!')
+    } else{
+        res.innerHTML = 'Contando: '
+        let inicio = Number(inui.value)
+        let fim = Number(ifim.value)
+        let passo = Number(ipasso.value)
+        if (passo <= 0){ 
+            //Confirmação de PASSO
+            window.alert('Passo inválido! Considerando PASSO 1')
+            passo = 1
+        }
+        if (inicio < fim){
+            // Contagem crescente
+            for(let c = inicio; c <= fim; c += passo){
+                        res.innerHTML += ` ${c} \u{1F449}`
+                    }
+        } else{
+            // Contagem regressiva
+            for(let c = inicio; c >= fim; c -= passo){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        
+        res.innerHTML += `\u{1F3C1}`
+        }
     }
-res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
-res.appendChild(img)
     
-}
